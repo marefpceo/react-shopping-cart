85dda4efead7964ff334aaa971f4aacaed6aa85d
marefpceo <32721274+marefpceo@users.noreply.github.com>
2023-09-29 22:15:33 -0700

    Completed Footer logic
    Added collapsable menus for Customer Service and Store Infomation.
    
    Added links to social media pages.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import plusIcon from '../../assets/plus-solid.png';
+import minusIcon from '../../assets/minus-solid.png';
 import Button from '../Button/Button';
 import facebookIcon from '../../assets/facebook.png';
 import instagramIcon from '../../assets/instagram.png';
@@ -6,19 +7,97 @@ import pinterestIcon from '../../assets/pinterest.png';
 import xIcon from '../../assets/x-twitter.png';
 import youtubeIcon from '../../assets/youtube.png';
 import '../Footer/Footer.css';
+import { useState } from 'react';
 
 function Footer() {
+  const [serviceList, setServiceList] = useState('0');
+  const [infoList, setInfoList] = useState('0');
+
+  function toggleServiceList() {
+    if (serviceList === '0') {
+      setServiceList('100%');
+    } else {
+      setServiceList('0');
+    }
+  }
+
+  function toggleInfoList() {
+    if (infoList === '0') {
+      setInfoList('100%');
+    } else {
+      setInfoList('0');
+    }
+  }
+
   return (
     <footer>
       <hr />
+
       <div className='footer-links'>
-        <h3>Customer Service</h3>
-        <img src={plusIcon} alt='' />
+        <div>
+          <h3>Customer Service</h3>
+          <img
+            src={serviceList === '100%' ? minusIcon : plusIcon}
+            alt='Customer service'
+            onClick={toggleServiceList}
+          />
+        </div>
+        <ul className='customer-service-list' style={{ height: serviceList }}>
+          <li>
+            <a href='#'>
+              <p>Contact Us</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>FAQs</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>Returns</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>Shipping</p>
+            </a>
+          </li>
+        </ul>
       </div>
       <hr />
+
       <div className='footer-links'>
-        <h3>Store Information</h3>
-        <img src={plusIcon} alt='' />
+        <div>
+          <h3>Store Information</h3>
+          <img
+            src={infoList === '100%' ? minusIcon : plusIcon}
+            alt='Store information'
+            onClick={toggleInfoList}
+          />
+        </div>
+        <ul className='store-info-list' style={{ height: infoList }}>
+          <li>
+            <a href='#'>
+              <p>Locations</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>Store Hours</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>Careers</p>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <p>Sitemap</p>
+            </a>
+          </li>
+        </ul>
       </div>
       <hr />
       <div className='email-signup'>
@@ -44,11 +123,21 @@ function Footer() {
       <div className='social-media'>
         <p>Connect with us on social media!</p>
         <div className='social-media-icons'>
-          <img src={facebookIcon} alt='' />
-          <img src={instagramIcon} alt='' />
-          <img src={pinterestIcon} alt='' />
-          <img src={xIcon} alt='' />
-          <img src={youtubeIcon} alt='' />
+          <a href='https://www.facebook.com' target='_blank' rel='noreferrer'>
+            <img src={facebookIcon} alt='Facebook' />
+          </a>
+          <a href='https://www.instagram.com' rel='noreferrer' target='_blank'>
+            <img src={instagramIcon} alt='Instagram' />
+          </a>
+          <a href='https://www.pinterest.com ' rel='noreferrer' target='_blank'>
+            <img src={pinterestIcon} alt='Pinterest' />
+          </a>
+          <a href='https://www.twitter.com' rel='noreferrer' target='_blank'>
+            <img src={xIcon} alt='Twitter' />
+          </a>
+          <a href='https://www.youtube.com' rel='noreferrer' target='_blank'>
+            <img src={youtubeIcon} alt='Youtube' />
+          </a>
         </div>
       </div>
     </footer>
