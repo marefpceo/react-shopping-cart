e7febcd07e105389e3eb6202e64856c4b3a0cde5
marefpceo <32721274+marefpceo@users.noreply.github.com>
2023-10-01 09:17:31 -0700

    Update remote with local
    Initial fetch was coded to test API.

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Card from '../Card/Card';
 import '../Shop/Shop.css';
 
 function Shop() {
+  const [productData, setProductData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch('https://dummyjson.com/products?limit=0');
+
+        if (response.status >= 400) {
+          throw new Error('Server error');
+        }
+        let responseData = await response.json();
+        setProductData(responseData);
+        setError(null);
+      } catch (error) {
+        setError(error);
+        setProductData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+    console.log(productData);
+  }, []);
+
   return (
     <>
       <Header />
