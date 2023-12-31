c2a1711e514dc43013cf964b6c54491b9b0c5131
marefpceo <marefpceo@gmail.com>
2023-12-31 15:20:31 -0800

    Updated Header to conditionally show category menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,15 @@ import Nav from '../Nav/Nav';
 import Menu from '../Menu/Menu';
 import menuIcon from '../../assets/bars-solid.png';
 import '../Header/Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header({ cartCount, updateCategory }) {
   const [displayState, setDisplayState] = useState('0%');
   const [showMenu, setShowMenu] = useState('0');
   const [scrollPosition, setScrollPosition] = useState(0);
+
+  const location = useLocation();
+  console.log(location.pathname);
 
   function handleClick() {
     if (displayState === '0%') {
@@ -44,43 +47,49 @@ function Header({ cartCount, updateCategory }) {
         </div>
         <Nav cartCount={cartCount} />
       </div>
-      <ul className='menu-lg'>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('smartphones')}>
-            <p>Smart Phones</p>
-          </Link>
-        </li>
-        <li>&bull;</li>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('laptops')}>
-            <p>Laptops</p>
-          </Link>
-        </li>
-        <li>&bull;</li>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('fragrances')}>
-            <p>Fragrances</p>
-          </Link>
-        </li>
-        <li>&bull;</li>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('skincare')}>
-            <p>Skin Care</p>
-          </Link>
-        </li>
-        <li>&bull;</li>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('groceries')}>
-            <p>Groceries</p>
-          </Link>
-        </li>
-        <li>&bull;</li>
-        <li>
-          <Link to={'/shop'} onClick={() => updateCategory('home-decoration')}>
-            <p>Home Decor</p>
-          </Link>
-        </li>
-      </ul>
+
+      {location.pathname === '/shop' ? (
+        <ul className='menu-lg'>
+          <li>
+            <Link to={'/shop'} onClick={() => updateCategory('smartphones')}>
+              <p>Smart Phones</p>
+            </Link>
+          </li>
+          <li>&bull;</li>
+          <li>
+            <Link to={'/shop'} onClick={() => updateCategory('laptops')}>
+              <p>Laptops</p>
+            </Link>
+          </li>
+          <li>&bull;</li>
+          <li>
+            <Link to={'/shop'} onClick={() => updateCategory('fragrances')}>
+              <p>Fragrances</p>
+            </Link>
+          </li>
+          <li>&bull;</li>
+          <li>
+            <Link to={'/shop'} onClick={() => updateCategory('skincare')}>
+              <p>Skin Care</p>
+            </Link>
+          </li>
+          <li>&bull;</li>
+          <li>
+            <Link to={'/shop'} onClick={() => updateCategory('groceries')}>
+              <p>Groceries</p>
+            </Link>
+          </li>
+          <li>&bull;</li>
+          <li>
+            <Link
+              to={'/shop'}
+              onClick={() => updateCategory('home-decoration')}
+            >
+              <p>Home Decor</p>
+            </Link>
+          </li>
+        </ul>
+      ) : null}
     </header>
   );
 }
